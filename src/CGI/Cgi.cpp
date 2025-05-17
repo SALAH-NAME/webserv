23c9f9f4be1e474562dccc866fea6a6c766a7674
medidbella <mohamedidbellaali@gmail.com>
2025-05-17 16:02:48 +0100

    cgi mvp

--- src/CGI/Cgi.cpp
+++ src/CGI/Cgi.cpp
@@ -16,9 +16,9 @@ void	prepare_cgi_env(Request	&http_req, Environment &my_env)
 	my_env.Add("SERVER_NAME=", http_req.server_name);
 	my_env.Add("SERVER_PORT=", num_to_string(http_req.server_port));
 	my_env.Add("SERVER_PROTOCOL=", "HTTP/1.1");
-	my_env.Add("SERVER_SOFTWARE=", "Ed, Edd n Eddy/1.0");
-	my_env.Add("CONTENT_LENGTH=", "");
-	my_env.Add("CONTENT_TYPE=", "");
+	my_env.Add("SERVER_SOFTWARE=", "Ed Edd n Eddy/1.0");
+	my_env.Add("CONTENT_LENGTH=", "non");
+	my_env.Add("CONTENT_TYPE=", "non");
 	my_env.Add("QUERY_STRING=", http_req.query_string);
 	my_env.Add("PATH_INFO=", http_req.path_info);
 	my_env.Add("REMOTE_ADDR=", http_req.client_addrs);
@@ -29,15 +29,17 @@ void	prepare_cgi_env(Request	&http_req, Environment &my_env)
 CgiHandler::CgiHandler(Request http_req, Environment my_env)
 {
 	int id;
-	char *tab[2];
-	tab[0] = (char *)http_req.script.c_str();
-	tab[1] = NULL;
+	char *tab[3];
+	tab[0] = (char *)"python3";
+	tab[1] = (char *)http_req.script.c_str();
+	tab[2] = NULL;
+	cout << "argv[0] = "<< tab[0] << endl;
+	cout << "cmd path =" << http_req.interpiter<< endl;
 	id = fork();
 	if (id != 0)//child
 	{
 		prepare_cgi_env(http_req, my_env);
-		cout << http_req.script<<endl;
 		execve(http_req.interpiter.c_str(), tab, my_env.GetRawEnv());
-		cerr << "ops\n";
+		cerr << "execve failed\n";
 	}
 }
--- src/CGI/test_script.py
+++ src/CGI/test_script.py
@@ -1,6 +1,15 @@
 import os
 
 env = str(os.environ)
-tab = env.split(',')
-for elm in tab:
-	print(elm)
+flag = False
+PrevChar = 'c'
+for c in str(env):
+	if PrevChar == '{':
+		flag = True
+	elif c == '}':
+		flag = False
+	if flag and c == ',':
+		print()
+	elif flag:
+			print(c, end='')
+	PrevChar = c
