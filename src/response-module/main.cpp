34aa2c33c2d368adfe304ef2cfecf71d1c3ce547
medidbella <mohamedidbellaali@gmail.com>
2025-05-28 16:51:01 +0100

    restart

--- src/response-module/main.cpp
+++ src/response-module/main.cpp
@@ -1,35 +1,9 @@
-// #include "Response.hpp"
-// #include "Request.hpp"
-
-#include "ResponseManager.hpp"
-
-void    print_line(std::string line) {
-	for (int i = 0; i < line.size(); i++) {
-		if (line[i] == '\r')
-			std::cout << "\\r";
-		else if (line[i] == '\n')
-			std::cout << "\\n\n";
-		else
-			std::cout << line[i];
-	}
-}
+#include "../../include/Request.hpp"
 
 
-void	RequestGenerator(Request &req) {
-	req.set_up("localhost", "GET", "/test.html", "HTTP/1.1", "close");
-}
 
-int main() {
-	Request	req;
 
-	RequestGenerator(req);
+int main()
+{
 
-	req.printer();
-	std::cout << "=========================\n\n";
-
-	ResponseManager Manager;
-	Manager.createResponse(req);
-	Manager.buildHttpResponse();
-
-	std::cout << std::endl;
 }
