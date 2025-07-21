648ca5ed5dcc46d898832b50409ac8291f985ce7
SALAH-NAME <ouahidi.se@proton.me>
2025-07-21 14:34:49 +0100

    update: httpRequest Parser, to validate Start-line and Headers line by line. add: new test for the new parser.

--- src/server/Client.cpp
+++ src/server/Client.cpp
@@ -16,147 +16,181 @@
 #include <ctime>
 #include "HttpRequest.hpp"
 
-Client::Client(Socket sock, int serverFD, const ServerConfig& conf) : _socket(sock), _serverSocketFD(serverFD),
-											_readBytes(0), _lastTimeConnection(std::time(NULL)),
-											_incomingDataDetected(INCOMING_DATA_OFF),
-											_responseInFlight(false), _sentBytes(0),
-											_isKeepAlive(true),
-											_availableResponseBytes(0),
-											_generateInProcess(GENERATE_RESPONSE_OFF)
-											, _responseHandler(new ResponseHandler(conf))
-{}
+Client::Client(Socket sock, int serverFD, const ServerConfig &conf) : _socket(sock), _serverSocketFD(serverFD),
+																	  _readBytes(0), _lastTimeConnection(std::time(NULL)),
+																	  _incomingDataDetected(INCOMING_DATA_OFF),
+																	  _responseInFlight(false), _sentBytes(0),
+																	  _isKeepAlive(true),
+																	  _availableResponseBytes(0),
+																	  _generateInProcess(GENERATE_RESPONSE_OFF), _responseHandler(new ResponseHandler(conf))
+{
+}
 
 Client::~Client() {}
 
-Socket&			Client::getSocket() {
+Socket &Client::getSocket()
+{
 	return _socket;
 }
-											
-size_t	Client::getReadBytes(void) {
+
+size_t Client::getReadBytes(void)
+{
 	return _readBytes;
 }
 
-int		Client::getServerSocketFD(void) {
+int Client::getServerSocketFD(void)
+{
 	return _serverSocketFD;
 }
 
-time_t		Client::getLastConnectionTime(void){
+time_t Client::getLastConnectionTime(void)
+{
 	return _lastTimeConnection;
 }
 
-bool	Client::getIncomingDataDetected(void) {
+bool Client::getIncomingDataDetected(void)
+{
 	return _incomingDataDetected;
 }
 
-bool	Client::getIsKeepAlive(void) {
+bool Client::getIsKeepAlive(void)
+{
 	return _isKeepAlive;
 }
 
-size_t	Client::getSentBytes(void) {
+size_t Client::getSentBytes(void)
+{
 	return _sentBytes;
 }
 
-int	Client::getBytesToSendNow(void) {
+int Client::getBytesToSendNow(void)
+{
 	if (_availableResponseBytes >= BYTES_TO_SEND)
 		return BYTES_TO_SEND;
 	return _availableResponseBytes;
 }
 
-bool	Client::getGenerateInProcess(void) {
+bool Client::getGenerateInProcess(void)
+{
 	return _generateInProcess;
 }
 
-HttpRequest&	Client::getHttpRequest(void) {
+HttpRequest &Client::getHttpRequest(void)
+{
 	return _httpRequest;
 }
 
-std::string&	Client::getHeaderPart(void) {
-	return	_requestHeaderPart;
+std::string &Client::getHeaderPart(void)
+{
+	return _requestHeaderPart;
 }
 
-std::string&	Client::getBodyPart(void) {
-	return	_requestBodyPart;
+std::string &Client::getBodyPart(void)
+{
+	return _requestBodyPart;
 }
 
-size_t	Client::getAvailableResponseBytes(void) {
+size_t Client::getAvailableResponseBytes(void)
+{
 	return _availableResponseBytes;
 }
 
-void	Client::setReadBytes(size_t bytes) {
+void Client::setReadBytes(size_t bytes)
+{
 	_readBytes += bytes;
 }
 
-void	Client::setResponseInFlight(bool value) {
+void Client::setResponseInFlight(bool value)
+{
 	_responseInFlight = value;
 }
 
-bool	Client::getResponseInFlight(void) {
+bool Client::getResponseInFlight(void)
+{
 	return _responseInFlight;
 }
 
-size_t	Client::getResponseSize(void) {
+size_t Client::getResponseSize(void)
+{
 	return _responseSize;
 }
 
-void		Client::appendToHeaderPart(const std::string& headerData) {
+void Client::appendToHeaderPart(const std::string &headerData)
+{
 	_requestHeaderPart += headerData;
 }
 
-void		Client::appendToBodyPart(const std::string& bodyData) {
+void Client::appendToBodyPart(const std::string &bodyData)
+{
 	_requestBodyPart += bodyData;
 }
 
-void	Client::setEvent(int _epfd, struct epoll_event& event) {
+void Client::setEvent(int _epfd, struct epoll_event &event)
+{
 	event.events = EPOLLIN | EPOLLOUT | EPOLLET;
 	epoll_ctl(_epfd, EPOLL_CTL_ADD, event.data.fd, &event);
 }
 
-void    Client::setServerSocketFD(int s_fd) {
+void Client::setServerSocketFD(int s_fd)
+{
 	_serverSocketFD = s_fd;
 }
 
-void	Client::setIncomingDataFlag(bool flag) {
+void Client::setIncomingDataFlag(bool flag)
+{
 	_incomingDataDetected = flag;
 }
 
-void	Client::resetLastConnectionTime(void){
+void Client::resetLastConnectionTime(void)
+{
 	_lastTimeConnection = std::time(NULL);
 }
 
-void	Client::setSentBytes(size_t bytes) {
+void Client::setSentBytes(size_t bytes)
+{
 	_sentBytes += bytes;
 	_availableResponseBytes -= bytes;
 }
 
-void	Client::resetSendBytes(void) {
+void Client::resetSendBytes(void)
+{
 	_sentBytes = 0;
 	_availableResponseBytes = 0;
 }
 
-void	Client::setIncomingDataDetected(int mode) {
+void Client::setIncomingDataDetected(int mode)
+{
 	_incomingDataDetected = mode;
 }
 
-void	Client::setGenerateResponseInProcess(bool value) {
+void Client::setGenerateResponseInProcess(bool value)
+{
 	_generateInProcess = value;
 }
 
-void	Client::setResponseSize(size_t size) {
+void Client::setResponseSize(size_t size)
+{
 	_responseSize = size;
 }
 
-void	Client::setAvailableResponseBytes(size_t value) {
+void Client::setAvailableResponseBytes(size_t value)
+{
 	_availableResponseBytes = value;
 }
 
-void	Client::clearRequestHolder(void) {
+void Client::clearRequestHolder(void)
+{
 	_requestHeaderPart.clear();
+	_requestBodyPart.clear();
+	_httpRequest.reset(); // Reset the incremental parser state
 }
 
-bool		Client::parseRequest() {
+bool Client::parseRequest()
+{
 	return _httpRequest.parse(_requestHeaderPart);
 }
 
-void	Client::prinfRequestinfos(void) {
+void Client::prinfRequestinfos(void)
+{
 	_httpRequest.printInfos();
 }
--- src/server/ReceiveRequests.cpp
+++ src/server/ReceiveRequests.cpp
@@ -13,66 +13,107 @@
 #include "Server.hpp"
 #include "ServerManager.hpp"
 
-void	isolateAndRecordBody(Client& client, std::string tempBuffer, size_t headerEnd) {
-	if (tempBuffer.size() == headerEnd + 4) {
+void isolateAndRecordBody(Client &client, std::string tempBuffer, size_t headerEnd)
+{
+	if (tempBuffer.size() == headerEnd + 4)
+	{
 		// no body-data received after header ==> no need to save
 		client.appendToHeaderPart(tempBuffer);
 		client.setReadBytes(tempBuffer.size());
-		return ;
+		return;
 	}
-	
+
 	// some body-data received after header-data ==> it needs to be saved and removed from header part
 	client.appendToHeaderPart(tempBuffer.substr(0, headerEnd + 4));
 	client.setReadBytes(headerEnd + 4);
 	client.appendToBodyPart(tempBuffer.substr(headerEnd + 4)); // here we save the body
-	
 }
 
-void    ServerManager::collectRequestData(Client& client, int serverIndex) {
+// new version
+void ServerManager::collectRequestData(Client &client, int serverIndex)
+{
 	int clientSocket = client.getSocket().getFd();
 	ssize_t readbytes;
 	size_t headerEnd;
 
+	(void)serverIndex, (void)clientSocket;
+
 	std::memset(_buffer, 0, sizeof(_buffer)); // use std
 
-	try {
-		readbytes = client.getSocket().recv((void*)_buffer, BYTES_TO_READ);
+	try
+	{
+		readbytes = client.getSocket().recv((void *)_buffer, BYTES_TO_READ);
 		// std::cout << "read bytes ==> " << readbytes << " from : " << client.getSocket().getFd() << "\n";
-		
-		if (readbytes > 0) {
+
+		if (readbytes > 0)
+		{
 			// printRequestAndResponse("Header", std::string(_buffer, readbytes));
 			client.resetLastConnectionTime();
-			if ((headerEnd = (std::string(_buffer, readbytes)).find(_2CRLF)) != std::string::npos) {
-				// std::cout << "   ====>> request is completed <<=====\n";
-				isolateAndRecordBody(client, std::string(_buffer, readbytes), headerEnd);
+
+			// Append new data to the header part
+			client.appendToHeaderPart(std::string(_buffer, readbytes));
+
+			// Validate and parse incrementally after each receive
+			HttpRequest &req = client.getHttpRequest();
+			req.appendAndValidate(std::string(_buffer, readbytes));
+
+			// Check if parsing is complete
+			if (req.getState() == HttpRequest::STATE_BODY)
+			{
+				// Check if there's body data after headers
+				if ((headerEnd = client.getHeaderPart().find(_2CRLF)) != std::string::npos)
+					isolateAndRecordBody(client, client.getHeaderPart(), headerEnd);
+
 				// printRequestAndResponse("Header", client.getHeaderPart());
 				// printRequestAndResponse("Body", client.getBodyPart());
-				if (client.parseRequest()) {
-					// client.prinfRequestinfos();
-					client.setIncomingDataDetected(INCOMING_DATA_OFF);
-					client.setGenerateResponseInProcess(GENERATE_RESPONSE_ON);
-				}
-				else
-					_servers[serverIndex].closeConnection(clientSocket);
+
+				client.setIncomingDataDetected(INCOMING_DATA_OFF);
+				client.setGenerateResponseInProcess(GENERATE_RESPONSE_ON);
+				// client.prinfRequestinfos();
 			}
-			else {
-				client.appendToHeaderPart(std::string(_buffer, readbytes));
+			else if (req.getState() == HttpRequest::STATE_ERROR)
+			{
+				std::string error_msg = "HTTP parsing error: " + req.getErrorMsg();
+				throw std::runtime_error(error_msg);
+			}
+			else
+			{
+				// Still waiting for more data
 				client.setReadBytes(readbytes);
 			}
 		}
 		else
+		{
 			throwIfSocketError("recv()");
-			// _servers[serverIndex].closeConnection(clientSocket);
-	} catch (const std::runtime_error& e) {
+		}
+	}
+	catch (const HttpRequestException &e)
+	{
+		std::string error_msg = "HTTP Request Error: ";
+		error_msg += e.what();
+		std::cerr << error_msg << std::endl;
+		_servers[serverIndex].closeConnection(clientSocket);
+	}
+	catch (const std::runtime_error &e)
+	{
 		perror(e.what());
+		_servers[serverIndex].closeConnection(clientSocket);
+	}
+	catch (const std::exception &e)
+	{
+		std::string error_msg = "Parsing error: ";
+		error_msg += e.what();
+		std::cerr << error_msg << std::endl;
 		_servers[serverIndex].closeConnection(clientSocket);
 	}
 }
 
-void	ServerManager::receiveClientsData(int serverIndex) {
-	std::map<int, Client>& clients = _servers[serverIndex].getClients();
+void ServerManager::receiveClientsData(int serverIndex)
+{
+	std::map<int, Client> &clients = _servers[serverIndex].getClients();
 
-	for (std::map<int, Client>::iterator it = clients.begin(); it != clients.end(); it++){
+	for (std::map<int, Client>::iterator it = clients.begin(); it != clients.end(); it++)
+	{
 		if (it->second.getIncomingDataDetected() == INCOMING_DATA_ON)
 			collectRequestData(it->second, serverIndex);
 	}
